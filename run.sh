d806ce37103e44bbdc01e3350028a5d4182ff38f
Catalin Stan <catstan@cisco.com>
2023-10-18 15:50:16 +0300

    stream app from run script

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
-stdbuf --output=L ./app/moonlight | tee -i x
+#stdbuf --output=L ./app/moonlight | tee -i x
+
 #gdb --args ./app/moonlight
+
+
+STREAM_ARGS="--quit-after"
+#STREAM_ARGS="--quit-after --packet-size 1392"
+#STREAM_ARGS="--quit-after --packet-size 32"
+#STREAM_ARGS="--no-quit-after"
+
+IP=37.50.143.245
+APP="Low Res Desktop"
+
+#./app/moonlight stream $IP "$APP"
+stdbuf --output=L ./app/moonlight $STREAM_ARGS stream $IP "$APP" | tee -i x
 
